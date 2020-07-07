728b8c1543767e313db03131e6c79bfac4655b61
Cobalt <Chaosthe0rie@protonmail.com>
2020-07-08 00:18:46 +0200

    fix typo

--- uglify.py
+++ uglify.py
@@ -3,6 +3,7 @@
 from pelican import signals
 from subprocess import check_call
 import logging
+from re import match
 import os
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,7 @@ Minify CSS and JS files in output path with uglifyjs and uglifycss.
 Required: an existing installation of uglifyjs/ uglifycss Compressor.
 """
 
+
 def minify(pelican):
     """
       Minify CSS and JS with uglifyjs/ uglifycss
@@ -19,19 +21,25 @@ def minify(pelican):
     """
     js = pelican.settings.get("UGLIFYJS_EXECUTABLE", "uglifyjs")
     css = pelican.settings.get("UGLIFYCSS_EXECUTABLE", "uglifycss")
-    js_options = pelican.settings.get("UGLIFYJS_OPTIONS", ["-c", "--source-map"])
+    js_options = pelican.settings.get(
+        "UGLIFYJS_OPTIONS", ["-c", "--source-map"])
     css_options = pelican.settings.get("UGLIFYCSS_OPTIONS", [])
+    exclude = pelican.settings.get("UGLIFY_EXCLUDE", [])
     for dirpath, _, filenames in os.walk(pelican.settings["OUTPUT_PATH"]):
         for name in filenames:
             _name = os.path.splitext(name)
-            if _name[1] == ".css" and _name[0][-3:] != "min":
+            if True in [match(regex, name) for regex in exclude]:
+                continue
+            elif _name[1] == ".css" and _name[0][-3:] != "min":
                 filepath = os.path.join(dirpath, name)
                 logger.info(f"minify {filepath}")
-                check_call([css, *css_options, filepath, "--output", f"{filepath[:-4]}.min.css"])
-            elif os.path.splitext(name)[1] == ".js" and  _name[0][-3:] != "min":
+                check_call([css, *css_options, filepath,
+                            "--output", f"{filepath[:-4]}.min.css"])
+            elif os.path.splitext(name)[1] == ".js" and _name[0][-3:] != "min":
                 filepath = os.path.join(dirpath, name)
                 logger.info(f"minify {filepath}")
-                check_call([js, *js_options, filepath, "-o", f"{filepath[:-3]}.min.js"])
+                check_call([js, *js_options, filepath,
+                            "-o", f"{filepath[:-3]}.min.js"])
 
 
 def register():
